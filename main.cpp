171978825b986b673ef2a932cb9e413b15dfc51a
Olivier <olivier.oblin@gmail.com>
2010-09-20 23:07:59 +0200

    lecture des parametres au lancement du programme

--- main.cpp
+++ main.cpp
@@ -1,9 +1,64 @@
+/****************************
+*                           *
+*           PONG            *
+*                           *
+*****************************/
+/**
+* Quentin Lebourgeois
+* Erwan Matrat
+* Charles Daumont
+* Olivier Blin
+**/
+
+////////// STL /////////////
 #include <iostream>
+#include <string>
+#include <vector>
+
+////////// SFML /////////////
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+// Constants
+#include "inc\constants.hpp"
+// Fonctions
+#include "inc\flux.hpp" // flux de convertion
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+// Prototypes
+void getParamProgram(int argc, char *argv[], std::string &fileName, int &speed);
+
+
+int main(int argc, char *argv[]) {
+    // initialisation des parametres
+    std::string nomFichier;
+    int vitesse;
+    getParamProgram(argc, argv, nomFichier, vitesse);
+    cout << "nom du fichier : " << nomFichier << "  vitesse : " << vitesse << endl;
     return 0;
+}
+
+void getParamProgram(int argc, char *argv[], std::string &fileName, int &speed) {
+// Déclaration des variables
+    string tmp, tmpVal;
+// initialisation des variables
+    fileName = "";
+    speed = 0;
+// Traitement
+    // parcourir le tableau de caracteres du main
+    for(int i = 0; i < argc; i++) {
+        tmp = argv[i];
+        // traitement du parametre de vitesse
+        if(tmp == "-v" && (i+1) < argc && speed == 0) {
+            tmpVal = argv[(i+1)];
+            speed = stringToInt(tmpVal);
+            i++; // pour ne pas retomber sur le i qui contient la valeur de la vitesse
+        }
+        else { // si ce n'est pas la premiere case du tableau (qui contient le nom du programme)
+            if(i > 0 && tmp.size() > 2 && fileName == "") {
+                fileName = tmp;
+            }
+        }
+    }
 }
